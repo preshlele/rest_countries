4f5080a7e0b407ec42f48722596948c0d38796e4
emmens4u <emmanuel.mensah@amalitech.org>
2022-06-20 08:17:22 +0800

    Navbar update

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import $ from 'jquery'
+
+
+$(document).ready(function () {
+    let modeContent = $('#mode').html();
+    console.log(modeContent);
+      
+        
+  
+  });
+  
