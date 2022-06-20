1c4471d47477d1baa36fe50a4fce90d7904b4ee8
emmens4u <emmanuel.mensah@amalitech.org>
2022-06-20 08:18:46 +0800

    Commented not working

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,4 +9,36 @@ $(document).ready(function () {
         
   
   });
-  
+  // $("#modeDiv").click(function () {
+    //   console.log($('#mode').html());
+    //   let modeContent = $('#mode').html();
+    //     if(modeContent === 'Light Mode'){
+    //       $('#mode').html('Dark Mode');
+    //       $("body").css("background-color","#F2F2F2");
+    //       $("body").css("color","#111517");
+    //       $("header").css("background-color","#FFFFFF");
+    //       $("header").css("color","#2B3844");
+    //       $("article").css("background-color","#FFFFFF");
+    //       $(".details").css("background-color","#FFFFFF");
+    //       $("#search").css("background-color","#FFFFFF");
+    //       $(".select").css("background-color","#FFFFFF");
+    //       $(".select").css("color","#2B3844");
+    //       $("#search").removeClass('placeHolder');
+        
+        
+    //     } else {
+    //       $('#mode').html('Light Mode');
+    //       $("body").css("background-color","#202C36");
+    //       $("body").css("color","#FFFFFF");
+    //       $("header").css("background-color","#2B3844");
+    //       $("header").css("color","#FFFFFF");
+    //       $("article").css("background-color","#2B3844");
+    //       $(".details").css("background-color","#2B3844");
+    //       $("#search").css("background-color","#2B3844");
+    //       $(".select").css("background-color","#2B3844");
+    //       $(".select").css("color","#FFFFFF");
+    //       $("#search").addClass('placeHolder');
+    
+        
+    //     }
+    // });
